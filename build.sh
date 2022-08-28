fa8da4911b9c5b47b98e3768d6e72c3c365d8c14
eggydutch <59375032+eggydutch@users.noreply.github.com>
2022-08-29 02:18:14 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
+@@ -1,41 +1,40 @@
+#!/bin/bash
+CRTDIR=$(pwd)
+base=$1
+profile=$2
+echo $base
+if [ ! -e "$base" ]; then
+	echo "Please enter base folder"
+	exit 1
+else
 	if [ ! -d $base ]; then 
 		echo "Openwrt base folder not exist"
 		exit 1
 	fi
 fi
 if [ ! -n "$profile" ]; then
-	profile=glinet-ax1800
+	profile=target_wlan_ap-gl-ax1800
 fi
 echo "Start..."
 #clone source tree 
@@ -13,12 +23,14 @@ cp -r custom/  $base/gl-infra-builder/feeds/custom/
 cp -r *.yml $base/gl-infra-builder/profiles
 cd $base/gl-infra-builder
 #setup
-python3 setup.py -c config-wlan-ap-5.4.yml
+if [[ $profile == *5-4* ]]; then
+        python3 setup.py -c config-wlan-ap-5.4.yml
+else
+        python3 setup.py -c config-wlan-ap.yml
+fi
+
 cd wlan-ap/openwrt
-./scripts/gen_config.py $profile glinet_depends
-
-git clone https://github.com/gl-inet/glinet.git $base/glinet
-
+./scripts/gen_config.py $profile glinet_depends custom
 git clone https://github.com/gl-inet/glinet4.x.git $base/glinet
 ./scripts/feeds update -a 
 ./scripts/feeds install -a
