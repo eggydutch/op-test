09492b7b0a00cb7b27d924dbd7c0dbb8931367c7
eggydutch <59375032+eggydutch@users.noreply.github.com>
2022-08-30 03:36:54 +0800

    Update diy-part1.sh

--- config/gl.inet/A1300/diy-part1.sh
+++ config/gl.inet/A1300/diy-part1.sh
@@ -44,30 +44,17 @@
 #git clone https://github.com/immortalwrt/luci/tree/openwrt-21.02/applications
 
 # Add Passwall
-#git clone --depth=1 -b packages https://github.com/xiaorouji/openwrt-passwall
+git clone --depth=1 -b packages https://github.com/xiaorouji/openwrt-passwall
 #git clone --depth=1 -b luci https://github.com/xiaorouji/openwrt-passwall
-#git clone --depth=1 -b main https://github.com/xiaorouji/openwrt-passwall2
+git clone --depth=1 -b main https://github.com/xiaorouji/openwrt-passwall2
 
 # Add OpenClash
-#git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 
 # Add luci-theme-argon
 #git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 #rm -rf ../lean/luci-theme-argon
 
-# Add cpufreq
-#git clone --depth=1 https://github.com/coolsnowwolf/lede/tree/master/package/lean/luci-app-cpufreq
-#git clone --depth=1 -b openwrt-18.06-k5.4 https://github.com/immortalwrt/immortalwrt/tree/openwrt-18.06-k5.4/package/lean/luci-app-cpufreq
-
 # Add VSSR(Hello World)
 #git clone https://github.com/jerrykuku/lua-maxminddb
 #git clone https://github.com/jerrykuku/luci-app-vssr
-
-# Add Luci App For Trojan-Go
-#git clone --depth=1 -b master https://github.com/frainzy1477/luci-app-trojan
-
-# Add luci-app-frpc
-#git clone --depth=1 -b https://github.com/immortalwrt/luci/tree/openwrt-21.02/applications/luci-app-frpc
-
-# Add luci-app-diskman
-#git clone --depth=1 -b https://github.com/immortalwrt/luci/tree/openwrt-21.02/applications/luci-app-diskman
