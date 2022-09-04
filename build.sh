8b1b89a0e5457d2a6bd77d950a53c1ab65067d0f
eggydutch <59375032+eggydutch@users.noreply.github.com>
2022-09-04 23:39:46 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
-@@ -1,41 +1,40 @@
 #!/bin/bash
+
+
 CRTDIR=$(pwd)
+
 base=$1
 profile=$2
+ui=$3
 echo $base
 if [ ! -e "$base" ]; then
 	echo "Please enter base folder"
@@ -13,26 +16,47 @@ else
 		exit 1
 	fi
 fi
+
 if [ ! -n "$profile" ]; then
-	profile=target_ipq40xx_gl-a1300
+	profile=target_wlan_ap-gl-ax1800
 fi
+
+if [ ! -n "$ui" ]; then
+        ui=true
+fi
+
+
 echo "Start..."
+
 #clone source tree 
 git clone https://github.com/gl-inet/gl-infra-builder.git $base/gl-infra-builder
 cp -r custom/  $base/gl-infra-builder/feeds/custom/
 cp -r *.yml $base/gl-infra-builder/profiles
 cd $base/gl-infra-builder
 #setup
+
 if [[ $profile == *5-4* ]]; then
-        python3 setup.py -c config-wlan-ap-5.4.yml
+        python3 setup.py -c configs/config-wlan-ap-5.4.yml
 else
-        python3 setup.py -c config-wlan-ap.yml
+        python3 setup.py -c configs/config-wlan-ap.yml
 fi
 
+
 cd wlan-ap/openwrt
-./scripts/gen_config.py $profile glinet_depends custom
+if [[ $ui == true  ]]; then 
+	./scripts/gen_config.py $profile glinet_depends custom
+else
+	./scripts/gen_config.py $profile openwrt_common luci custom
+fi
+
+
 git clone https://github.com/gl-inet/glinet4.x.git $base/glinet
 ./scripts/feeds update -a 
 ./scripts/feeds install -a
 make defconfig
-make -j$(expr $(nproc) + 1) GL_PKGDIR=$base/glinet/ipq60xx/ V=s
+
+if [[ $ui == true  ]]; then 
+	make -j$(expr $(nproc) + 1) GL_PKGDIR=$base/glinet/ipq60xx/ V=s
+else
+	make -j$(expr $(nproc) + 1)  V=s
+fi
